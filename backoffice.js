8fd9d6a62b7d4c800d4c1d10fc8a54d86af5ca8c
alalaed <a.alaeddine@live.com>
2022-02-04 16:58:07 +0100

    submi button changeed

--- backoffice.js
+++ backoffice.js
@@ -1,30 +1,33 @@
 
 
 
-let url = "https://striveschool-api.herokuapp.com/api/movies/61fd4148e1d6d30015c29803"
+let url = "https://striveschool-api.herokuapp.com/api/movies/"
 let methode="POST"
 const key = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjM5OTgyZWExZDAwMTViYjAzZTkiLCJpYXQiOjE2NDM3OTk0NTAsImV4cCI6MTY0NTAwOTA1MH0.5dZKYBo7eP4on-pcM8VC0B42JNHHlwPOAqk70FWRG1M"
 let id = new URLSearchParams(window.location.search).get("Id")
 
-if(id){
+
+if (id) {
     methode="PUT"
     console.log("this id is ",id)
     url = url + id
-    console.log( url )
+    let submitToEdit = document.getElementById("submit").innerText="Edit"
+
     fetch( url  ,{
-        method:"PUT",
+        method: methode,
         headers:{
-            
-            "Authorization":key
+            "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjM5OTgyZWExZDAwMTViYjAzZTkiLCJpYXQiOjE2NDM3OTk0NTAsImV4cCI6MTY0NTAwOTA1MH0.5dZKYBo7eP4on-pcM8VC0B42JNHHlwPOAqk70FWRG1M"
         },
     }).then( resp => resp.json())
-    .then(data => {
-        console.log("this movie",data)
+    .then( data => {
+        console.log("this movie",data.name)
         const name = document.getElementById("name").value = data.name
         const description = document.getElementById("description").value = data.description
         const category = document.getElementById("category").value = data.category
         const imageUrl = document.getElementById("image-Url").value = data.imageUrl
-    }).catch(e => { console.log(e) })
+    })
+    
+    // .catch(e => { console.log(e) })
 
 }
                     
