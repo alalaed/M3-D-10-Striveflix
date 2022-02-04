9d5638e3d071432041f41dd4cb70314a78803f79
alalaed <a.alaeddine@live.com>
2022-02-04 16:43:33 +0100

    put request

--- backoffice.js
+++ backoffice.js
@@ -1,12 +1,34 @@
 
 
 
-const url = "https://striveschool-api.herokuapp.com/api/movies/"
-// method="POST"
+let url = "https://striveschool-api.herokuapp.com/api/movies/61fd4148e1d6d30015c29803"
+let methode="POST"
 const key = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjM5OTgyZWExZDAwMTViYjAzZTkiLCJpYXQiOjE2NDM3OTk0NTAsImV4cCI6MTY0NTAwOTA1MH0.5dZKYBo7eP4on-pcM8VC0B42JNHHlwPOAqk70FWRG1M"
-const id = new URLSearchParams(window.location.search).get("Id")
-method:"POST"
+let id = new URLSearchParams(window.location.search).get("Id")
 
+if(id){
+    methode="PUT"
+    console.log("this id is ",id)
+    url = url + id
+    console.log( url )
+    fetch( url  ,{
+        method:"PUT",
+        headers:{
+            
+            "Authorization":key
+        },
+    }).then( resp => resp.json())
+    .then(data => {
+        console.log("this movie",data)
+        const name = document.getElementById("name").value = data.name
+        const description = document.getElementById("description").value = data.description
+        const category = document.getElementById("category").value = data.category
+        const imageUrl = document.getElementById("image-Url").value = data.imageUrl
+    }).catch(e => { console.log(e) })
+
+}
+                    
+                
 
 
 const create = () => {
@@ -24,7 +46,7 @@ const create = () => {
     }
     
         fetch(url, {
-            method: "POST",
+            method: method,
             body: JSON.stringify(movie),
             headers: {
                 "Content-Type": "application/json",
